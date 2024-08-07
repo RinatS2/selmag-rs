5080903d048bd1347ca41d029a43edf7a6024573
Rinat Suleimanov <rinravis@gmail.com>
2024-08-07 19:49:36 +0300

    SC24EP01

---
+++ manager-app/src/main/java/com/suleimanov/manager/entity/Product.java
@@ -0,0 +1,19 @@
+package com.suleimanov.manager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    private Integer id;
+
+    private  String title;
+
+    private String details;
+
+
+}
